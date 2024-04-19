5cc695977774643887b9e3de2737704a0cc7a2f1
elizabethgeraldine <94831720+laoderidho@users.noreply.github.com>
2024-04-19 12:58:22 +0700

    add and install next js

---
+++ typescriptKeyword/bundle/index.js
@@ -0,0 +1,14 @@
+"use strict";
+// penamaan variable di sertai tipe data
+let username = "Ridho Fahreza";
+let age = 20.8;
+console.log(username, age);
+let arr;
+arr = ["Ridho", "Fahreza", 10];
+const Ridho = {
+    name: "Ridho",
+    age: 20,
+    address: "Kp. Cibaduyut",
+    major: "Teknik Informatika"
+};
+console.log(Ridho);
---
+++ typescriptKeyword/src/index.ts
@@ -0,0 +1,31 @@
+
+// penamaan variable di sertai tipe data
+let username: string = "Ridho Fahreza";
+
+let age: number = 20.8;
+
+console.log(username, age);
+
+
+let arr: any[];
+
+arr = ["Ridho", "Fahreza", 10];
+
+// object
+
+type Mhs = {
+    name: string
+    age: number
+    address: string
+    major: string
+}
+const Ridho: Mhs = {
+    name: "Ridho",
+    age: 20,
+    address: "Kp. Cibaduyut",
+    major: "Teknik Informatika"
+}
+
+console.log(Ridho);
+
+
